3ff700c328a510a6bf163d594f7565bcbfde77e8
skinner969 <skinner969@gmail.com>
2017-05-12 02:20:11 +0300

    fixes

--- src/test/java/com/example/rp/steps/MyStepdefs.java
+++ src/test/java/com/example/rp/steps/MyStepdefs.java
@@ -2,23 +2,26 @@ package com.example.rp.steps;
 
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class MyStepdefs {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MyStepdefs.class);
+    private static final Logger LOGGER = LogManager.getLogger(MyStepdefs.class);
 
     @When("^some random step$")
     public void someRandomStep() throws Throwable {
-        LOGGER.error("SOME RANDOM MESSAGE", "SOME RANDOM MESSAGE", "SOME RANDOM MESSAGE");
+        for (int k = 0; k < 3; k++) {
+            Thread.sleep(3000);
+            LOGGER.error("SOME RANDOM MESSAGE");
+        }
     }
 
     @Then("^random assertion$")
     public void randomAssertionStep() throws Throwable {
         Assert.assertTrue(true);
-        LOGGER.error("SUCCESS", "SUCCESS", "SUCCESS");
+        LOGGER.error("SUCCESS");
     }
 
 }
